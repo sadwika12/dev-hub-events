992dfd9e42f55820fb48f3355551434a12eb32ee
sadwika <n220049@rguktn.ac.in>
2026-03-16 22:18:37 +0530

    Implemented Database model and connection

---
+++ Database/index.ts
@@ -0,0 +1,7 @@
+// Models
+export { default as Event } from './event_model'
+export { default as Booking } from './booking_model'
+
+// TypeScript Interfaces
+export type { IEvent } from './event_model'
+export type { IBooking } from './booking_model'
