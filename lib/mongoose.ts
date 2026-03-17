289bb105e4b27f3281ae0ed97e87b89652334a47
sadwika <n220049@rguktn.ac.in>
2026-03-17 19:31:17 +0530

    Implememnted Database Connection

---
+++ lib/mongoose.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+type MongooseCache={
+  conn:typeof mongoose|null;
+  promise:Promise<typeof mongoose>|null;
+}
+declare global{
+  var mongoose:MongooseCache|undefined;
+}
+const MONGODB_URI=process.env.MONGODB_URI
+if(!MONGODB_URI){
+  throw new Error(
+    'Please define the MongoDB_URI'
+  );
+}
+let cached:MongooseCache=global.mongoose||{conn:null,promise:null}
+if(!global.mongoose){
+  global.mongoose=cached
+}
+
+async function connectDB():Promise<typeof mongoose>{
+  if(cached.conn){
+    return cached.conn
+  }
+  if(!cached.promise){
+    if(!MONGODB_URI){
+      throw new Error(
+        'Please define the MONOGODB_URI in the .env file'
+      );
+    }
+    const options = {
+      bufferCommands: false, 
+    };
+    cached.promise = mongoose.connect(MONGODB_URI!, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+
+
